846d7b8d0187e55d387c7056074ab9f0f190c0f9
Philip Metzger <philipmetzger@bluewin.ch>
2026-06-03 19:32:02 +0200

    core: Move `repo_path` into it
    This part of the library is quite basic and it doesn't have to many dependencies which also need to 
    move to make it happen. While we're at it finally finish writing its documentation.
    
    Part of #6284

--- core/src/lib.rs
+++ core/src/lib.rs
@@ -12,17 +12,35 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
-}
+//! The core library powering the Jujutsu Version Control System. It contains
+//! all "base" types such as `Commit` and the `Backend` trait.
+// Needed so that proc macros can be used inside jj_lib and by external crates
+// that depend on it.
+// See:
+// - https://github.com/rust-lang/rust/issues/54647#issuecomment-432015102
+// - https://github.com/rust-lang/rust/issues/54363
+extern crate self as jj_core;
+
+#[macro_use]
+pub mod content_hash;
+
+pub mod file_util;
+pub mod repo_path;
 
 #[cfg(test)]
 mod tests {
-    use super::*;
+    use tempfile::TempDir;
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+    // TODO: is this needed?
+    // Copied from `testutils::TestResult` to remove dependency cycle.
+    // pub type TestResult<T = ()> = eyre::Result<T>;
+
+    /// Unlike `testutils::new_temp_dir()`, this function doesn't set up
+    /// hermetic Git environment.
+    pub fn new_temp_dir() -> TempDir {
+        tempfile::Builder::new()
+            .prefix("jj-test-")
+            .tempdir()
+            .unwrap()
     }
 }
